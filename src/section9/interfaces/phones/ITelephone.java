352e7e8cadecd851ed1e6a0beeb86016d56c5950
acneuromancer <dokukodo108@gmail.com>
2019-06-16 18:36:03 +0200

    Add interface examples.

---
+++ src/section9/interfaces/phones/ITelephone.java
@@ -0,0 +1,18 @@
+package section9.interfaces.phones;
+
+public interface ITelephone {
+    // We create just the signatures (return type, name, parameters), we do not define the actual code itself.
+    // That goes into the classes that implement this interface.
+    // The default access modifier is public, it does not need writing explicitly.
+
+    void powerOn();
+
+    void dial(int phoneNumber);
+
+    void answer();
+
+    boolean callPhone(int phoneNumber);
+
+    boolean isRinging();
+
+}
