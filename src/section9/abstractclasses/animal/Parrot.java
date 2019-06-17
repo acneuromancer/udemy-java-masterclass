b69d13b59b40c5f55c6d06074daf7f9de1560229
acneuromancer <dokukodo108@gmail.com>
2019-06-18 00:32:56 +0200

    Add examples of abstract classes and their inheritance.

---
+++ src/section9/abstractclasses/animal/Parrot.java
@@ -0,0 +1,14 @@
+package section9.abstractclasses.animal;
+
+public class Parrot extends Bird {
+
+    public Parrot(String name) {
+        super(name);
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Flitting from branch to branch.");
+    }
+
+}
