1b105bbc3f0ec4a2807f0086f855a13cf5b2b353
acneuromancer <dokukodo108@gmail.com>
2019-07-13 20:03:32 +0200

    Add set challenge.

---
+++ src/section12/sets/challenge/Moon.java
@@ -0,0 +1,9 @@
+package section12.sets.challenge;
+
+public class Moon extends HeavenlyBody {
+
+    public Moon(String name, double orbitalPeriod) {
+        super(name, orbitalPeriod, BodyTypes.MOON);
+    }
+
+}
