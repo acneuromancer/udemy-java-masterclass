7453a3083e49f25deedffc0eef35ac57e3f8ec57
acneuromancer <dokukodo108@gmail.com>
2019-09-10 00:34:22 +0200

    Add an example of using file separators.

---
+++ src/section14/_09_filesystem/Separators.java
@@ -0,0 +1,34 @@
+package section14._09_filesystem;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+
+public class Separators {
+
+    public static void main(String[] args) {
+        String separator = File.separator;
+        System.out.println("Separator: " + separator);
+
+        separator = FileSystems.getDefault().getSeparator();
+        System.out.println("Separator: " + separator);
+
+        DirectoryStream.Filter<Path> filter = new DirectoryStream.Filter<Path>() {
+            @Override
+            public boolean accept(Path path) throws IOException {
+                return Files.isRegularFile(path);
+            }
+        };
+
+        Path directory = FileSystems.getDefault().getPath("Examples" + File.separator + "Dir1");
+
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, filter)) {
+            for (Path file : contents) {
+                System.out.println(file.getFileName());
+            }
+        } catch (IOException | DirectoryIteratorException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
