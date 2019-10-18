c74e78f72ffff0a8c95e7ea74d873f142a30e862
acneuromancer <dokukodo108@gmail.com>
2019-10-19 00:46:16 +0200

    Add an example of using the Function interface.

---
+++ src/section_16/_05_functionalinterface/function/Main.java
@@ -0,0 +1,55 @@
+package section_16._05_functionalinterface.function;
+
+import section_16._05_functionalinterface.predicate.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Function;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Employee john = new Employee("John Doe", 30);
+        Employee tim = new Employee("Tim Buchalka", 21);
+        Employee jack = new Employee("Jack Hill", 40);
+        Employee snow = new Employee("Snow White", 22);
+        Employee red = new Employee("Red Riding Hood", 35);
+        Employee charming = new Employee("Prince Charming", 31);
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(john);
+        employees.add(tim);
+        employees.add(jack);
+        employees.add(snow);
+        employees.add(red);
+        employees.add(charming);
+
+        employees.forEach(employee -> {
+            String lastName = employee.getName().substring(employee.getName().indexOf(' ') + 1);
+            System.out.println(lastName);
+        });
+
+        Function<Employee, String> getLastName = (Employee employee) -> {
+            return employee.getName().substring(employee.getName().indexOf(' ') + 1);
+        };
+
+        Function<Employee, String> getFirstName = (Employee employee) -> {
+            return employee.getName().substring(0, employee.getName().indexOf(' '));
+        };
+
+        Random random = new Random();
+        for (Employee employee : employees) {
+            if (random.nextBoolean()) {
+                System.out.println("First name: " + getAName(getFirstName, employee));
+            } else {
+                System.out.println("Last name: " + getAName(getLastName, employee));
+            }
+        }
+    }
+
+    private static String getAName(Function<Employee, String> getName, Employee employee) {
+        return getName.apply(employee);
+    }
+
+}
