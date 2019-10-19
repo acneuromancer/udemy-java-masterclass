5b58026dfc2c49c9d68477395ea29a986a128bd6
acneuromancer <dokukodo108@gmail.com>
2019-10-19 16:58:49 +0200

    Add an example of chaining functional interfaces.

---
+++ src/section_16/_05_functionalinterface/chaining/ChainingFunctionalInterfaces.java
@@ -0,0 +1,34 @@
+package section_16._05_functionalinterface.chaining;
+
+import section_16._05_functionalinterface.predicate.Employee;
+
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.IntUnaryOperator;
+
+public class ChainingFunctionalInterfaces {
+
+    public static void main(String[] args) {
+        Employee e1 = new Employee("John Doe", 21);
+        Function<Employee, String> upperCase = employee -> employee.getName().toUpperCase();
+        Function<String, String> firstName = name -> name.substring(0, name.indexOf(' '));
+        Function<Employee, String> chainedFunction = upperCase.andThen(firstName);
+        System.out.println(chainedFunction.apply(e1));
+
+
+        IntUnaryOperator incBy5 = i -> i + 5;
+        System.out.println(incBy5.applyAsInt(10));
+
+        Consumer<String> c1 = s -> s.toUpperCase();
+        Consumer<String> c2 = s -> System.out.println(s);
+        c1.andThen(c2).accept("Hello World!");
+
+        BiFunction<String, Employee, String> concatAge = (String name, Employee employee) -> {
+            return name.concat(" " + employee.getAge());
+        };
+        String upperName = upperCase.apply(e1);
+        System.out.println(concatAge.apply(upperName, e1));
+    }
+
+}
