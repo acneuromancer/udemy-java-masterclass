e53335a6e11d11de4bf255927ee6a695241b2285
acneuromancer <dokukodo108@gmail.com>
2019-10-21 00:23:21 +0200

    Add the solutions of a part of the lambda challenges.

---
+++ src/section_16/challanges/Challenge06.java
@@ -0,0 +1,18 @@
+package section_16.challanges;
+
+import java.util.function.Supplier;
+
+public class Challenge06 {
+
+    public static void main(String[] args) {
+        Supplier<String> iLoveJava = () -> "I Love Java";
+
+        iLoveJava = () -> {
+            return "I Love Java";
+        };
+
+        String supplierResult = iLoveJava.get();
+        System.out.println(supplierResult);
+    }
+
+}
