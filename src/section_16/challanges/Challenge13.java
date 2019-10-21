f9b0be18124b2500afd252f361bca85b53816846
acneuromancer <dokukodo108@gmail.com>
2019-10-21 22:00:29 +0200

    Add the solution of Challenge 13: stream chains are evaluated lazily. Nothing happens until a terminal operation is added to the chain. At that point, the chain is executed.

---
+++ src/section_16/challanges/Challenge13.java
@@ -0,0 +1,20 @@
+package section_16.challanges;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Challenge13 {
+
+    public static void main(String[] args) {
+        List<String> topNames2015 = Arrays.asList(
+                "Amelia", "Olivia", "emily", "Isla", "Ava", "oliver", "Jack", "Charlie", "harry", "Jacob"
+        );
+
+        topNames2015
+                .stream()
+                .map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
+                .peek(System.out::println)
+                .sorted(String::compareTo)
+                .forEach(System.out::println);
+    }
+}
