036e3d526631218bdf0d3e082e2101b5d8f3b4ae
acneuromancer <dokukodo108@gmail.com>
2019-10-27 12:25:33 +0100

    Add examples of using and, or, not operators in regex, and valide USA phone numbers and credit card numbers using a regex.

---
+++ src/section_17/_04_andornot/Main.java
@@ -0,0 +1,60 @@
+package section_17._04_andornot;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // And is the implicit logical operator: "abc" means "a" and "b" and "c"
+
+        // Or: [Hh] (or [H|h]
+        System.out.println("harry".replaceAll("[Hh]arry", "Larry"));
+        System.out.println("Harry".replaceAll("[H|h]arry", "Larry"));
+
+        // Not:^
+        // [^abc] - matches all characters except a, b and c.
+        String tvTest = "tstvtkt";
+
+        // Carrot expects a character after t
+        // String tNotVRegExp = "t[^v]";
+
+        // ! Negative look ahead operator
+        // Positive look ahead: t(?=v) - matches a t followed by a v, but the v is not part of the match
+        String tNotVRegExp = "t(?!v)";
+        Pattern tNotVPattern = Pattern.compile(tNotVRegExp);
+        Matcher tNotVMatcher = tNotVPattern.matcher(tvTest);
+
+        int count = 0;
+        while(tNotVMatcher.find()) {
+            count++;
+            System.out.println("Occurence " + count + ": " + tNotVMatcher.start() + " to " + tNotVMatcher.end());
+        }
+
+        // Validate a USA phone number
+        // ^([\(]{1}[0-9]{3}[\)]{1}[ ]{1}[0-9]{3}[\-]{1}[0-9]{4})$
+        String phone1 = "1234567890";  // Shouldn't match
+        String phone2 = "(123) 456-7890"; // match
+        String phone3 = "123 456-7890"; // Shouldn't match
+        String phone4 = "(123)456-7890"; // Shouldn't match
+
+        System.out.println("phone1 = " + phone1.matches("^([\\(]{1}[0-9]{3}[\\)]{1}[ ]{1}[0-9]{3}[\\-]{1}[0-9]{4})$"));
+        System.out.println("phone2 = " + phone2.matches("^([\\(]{1}[0-9]{3}[\\)]{1}[ ]{1}[0-9]{3}[\\-]{1}[0-9]{4})$"));
+        System.out.println("phone3 = " + phone1.matches("^([\\(]{1}[0-9]{3}[\\)]{1}[ ]{1}[0-9]{3}[\\-]{1}[0-9]{4})$"));
+        System.out.println("phone4 = " + phone1.matches("^([\\(]{1}[0-9]{3}[\\)]{1}[ ]{1}[0-9]{3}[\\-]{1}[0-9]{4})$"));
+
+        // Validate a credit card
+        // ^4[0-9]{12}([0-9]{3})?$
+        String visa1 = "4444444444444"; // should match
+        String visa2 = "5444444444444"; // shouldn't match
+        String visa3 = "4444444444444444";  // should match
+        String visa4 = "4444";  // shouldn't match
+
+        System.out.println("visa1 = " + visa1.matches("^4[0-9]{12}([0-9]{3})?$"));
+        System.out.println("visa2 = " + visa2.matches("^4[0-9]{12}([0-9]{3})?$"));
+        System.out.println("visa3 = " + visa3.matches("^4[0-9]{12}([0-9]{3})?$"));
+        System.out.println("visa4 = " + visa4.matches("^4[0-9]{12}([0-9]{3})?$"));
+    }
+
+}
