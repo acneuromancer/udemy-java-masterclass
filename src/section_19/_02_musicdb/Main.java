2541a1cd80fd739b72cfed3cc7276afe1c0de470
acneuromancer <dokukodo108@gmail.com>
2019-11-16 11:00:13 +0100

    Add query the artist for a song.

--- src/section_19/_02_musicdb/Main.java
+++ src/section_19/_02_musicdb/Main.java
@@ -2,32 +2,57 @@ package section_19._02_musicdb;
 
 import section_19._02_musicdb.model.Artist;
 import section_19._02_musicdb.model.DataSource;
+import section_19._02_musicdb.model.SongArtist;
 
 import java.util.List;
 
 public class Main {
+    private static DataSource dataSource;
+
+    static {
+        dataSource = new DataSource();
+    }
 
     public static void main(String[] args) {
-        DataSource dataSource = new DataSource();
-
         if (!dataSource.open()) {
             System.out.println("Can't open datasource.");
             return;
         }
 
+        dataSource.querySongsMetaData();
+
+        dataSource.close();
+    }
+
+    private static void query1() {
         List<Artist> artists = dataSource.queryArtist(DataSource.ORDER_BY_ASC);
         if (artists == null) {
             System.out.println("No artists!");
-            return;
+        } else {
+            artists.forEach(System.out::println);
         }
+    }
 
-        artists.forEach(System.out::println);
-
+    private static void query2() {
         List<String> albumsForArtist = dataSource.queryAlbumsForArtist("Iron Maiden", DataSource.ORDER_BY_ASC);
+        if (albumsForArtist == null) {
+            System.out.println("No albums!");
+        } else {
+            albumsForArtist.forEach(System.out::println);
+        }
+    }
 
-        albumsForArtist.forEach(System.out::println);
-
-        dataSource.close();
+    private static void query3() {
+        List<SongArtist> songArtists = dataSource.queryArtistForSong("Go Your Own Way", DataSource.ORDER_BY_ASC);
+        if (songArtists == null) {
+            System.out.println("Couldn't find the artist for the song");
+        } else {
+            for (SongArtist artist : songArtists) {
+                System.out.println("Artist name = " + artist.getArtistName() +
+                        ", album name = " + artist.getAlbumName() +
+                        ", track = " + artist.getTrack());
+            }
+        }
     }
 
 }
