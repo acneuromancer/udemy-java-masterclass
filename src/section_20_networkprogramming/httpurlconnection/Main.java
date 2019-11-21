f8ec43286fc93a52439c6f9fec0224d8be337edc
acneuromancer <dokukodo108@gmail.com>
2019-11-21 23:05:03 +0100

    Add an example of HttpURLConnection.

---
+++ src/section_20_networkprogramming/httpurlconnection/Main.java
@@ -0,0 +1,47 @@
+package section_20_networkprogramming.httpurlconnection;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("http://example.org");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET"); // GET is the default.
+            connection.setRequestProperty("User-Agent", "Chrome");
+            connection.setReadTimeout(30000);
+
+            // getResponseCode() method implicitly performs the connection.
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response code: " + responseCode);
+
+            if (responseCode != 200) {
+                System.out.println("Error reading web page");
+                return;
+            }
+
+            // The getInputStream() method implicitly connects.
+            BufferedReader inputReader = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream())
+            );
+
+            String line = "";
+            while ((line = inputReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            inputReader.close();
+        } catch (MalformedURLException e) {
+            System.out.println("Malformed URL: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
+
+}
